eaacbbaab8b72ffdd0bce43b30dba59f153af796
ismailSoylu <ismailsoylu0405@gmail.com>
2025-09-04 18:33:55 +0300

    fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.novaorion.volumecontrol"
         minSdk = 25
         targetSdk = 35
-    versionCode = 18
-        versionName = "1.14"
+    versionCode = 19
+        versionName = "1.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
---
+++ kaynak.md
@@ -0,0 +1,4 @@
+.\gradlew assembleDebug
+adb install -r "C:\Users\sarma\StudioProjects\VoiceButton\app\build\outputs\apk\apk\debug\app-apk-debug.apk"
+./gradlew bundleRelease
+C:\Users\sarma\StudioProjects\VoiceButton\app\build\outputs\bundle\release\app-release.aab
